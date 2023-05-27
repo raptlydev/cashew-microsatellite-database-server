15c30015488dd4e9503945aaf00b8b95a479b825
mainak92 <31222142+mainak92@users.noreply.github.com>
2023-05-27 17:15:45 +0530

    server initial commit

---
+++ functions/api.js
@@ -0,0 +1,30 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import router from './routes/posts.js';
+import path from 'path'
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+console.log(__dirname, "DIR NAME");
+const app = express();
+dotenv.config();
+app.use(bodyParser.json({ limit: "30mb", extended: true}));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true}));
+app.use(cors());
+
+app.use('/api', router);
+
+app.get('/', (req,res)=>{
+    res.send("Hello to the test projects")
+});
+// const CONNECTION_URL = "mongodb+srv://raptly:raptly123@cluster0.v9gql.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const PORT = process.env.PORT || 9000;
+
+// app.listen(PORT, ()=>console.log(`Server running on port: ${PORT}`))
+mongoose.connect(process.env.CONNECTION_URL)
+    .then(()=>app.listen(PORT, ()=>console.log(`Server running on port: ${PORT}`)))
+    .catch((err)=> console.log(err.message));
