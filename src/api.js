176e088d80b61aa58dda2119c6a0970915efc2b5
mainak92 <31222142+mainak92@users.noreply.github.com>
2023-06-04 19:21:03 +0530

    Initial commit

--- src/api.js
+++ src/api.js
@@ -1,17 +1,24 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import mongoose from 'mongoose';
-import cors from 'cors';
-import dotenv from 'dotenv';
-import router from './routes/posts.js';
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const dotenv = require('dotenv');
+// const postRoutes = require('../routes/posts.js');
+// const serverless = require('serverless-http');
 
+import router from '../routes/posts.js';
 const app = express();
 dotenv.config();
+
 app.use(bodyParser.json({ limit: "30mb", extended: true}));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true}));
 app.use(cors());
 
 app.use('/.netlify/functions/api', router);
+
+// app.get('*', (req,res)=>{
+//     req.sendFile(path.resolve(__dirname, 'build', 'index.html'))
+// });
 
 app.get('/', (req,res)=>{
     res.send("Hello to the test projects")
@@ -23,3 +30,5 @@ const PORT = process.env.PORT || 9000;
 mongoose.connect(process.env.CONNECTION_URL)
     .then(()=>app.listen(PORT, ()=>console.log(`Server running on port: ${PORT}`)))
     .catch((err)=> console.log(err.message));
+
+// module.exports.handler = serverless(app);
